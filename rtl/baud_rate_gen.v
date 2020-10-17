7d51e8fb6701d05c745b9f08692c0f62100a5847
Scrawach <Scrawach@gmail.com>
2020-10-17 21:24:41 +0300

    Baud rate generator refactoring.
    Create more readable RTL-code.

--- rtl/baud_rate_gen.v
+++ rtl/baud_rate_gen.v
@@ -24,15 +24,17 @@ module baud_rate_gen
 
   // ------------
   // Internal wires
-  wire       cpol; // clock polarity (selected SPI mode)
-  wire       cpha; // clock phase    (selected SPI mode)
-  wire       strobe; // strobe for generate SCLK
-
+  wire       cpol;    // clock polarity (selected SPI mode)
+  wire       cpha;    // clock phase    (selected SPI mode)
+  wire       ref_clk;
   // ------------
   // Internal registers
-  reg        sclk_en; // SPI clock enable generation
-  reg        sclk_reg; // SPI clock register
-  wire       sclk_w;
+  reg [2:0] cnt, cnt_nxt;
+  reg [2:0] sel_cnt;
+  reg       strobe;
+  reg       ref_cpha_0; // reference 50% duty cycle clock by selected strobes (pha = 0)
+  reg       ref_cpha_1; // reference 50% duty cycle clock by selected strobes (pha = 1)
+
   
   // ------------
   // MODULE IMPLEMENTATION
@@ -43,6 +45,75 @@ module baud_rate_gen
   assign cpha = mode[0];
 
   // ------------
+  // COUNTER FOR CLOCK DIVIDE
+  always @ ( posedge clk or negedge rst_n ) begin
+    if ( rst_n == 1'b0 ) begin
+      cnt <= 3'h0;
+    end else begin
+      cnt <= cnt_nxt;
+    end
+  end
+
+  always @ ( * ) begin
+    if ( strobe      ) begin
+      cnt_nxt = 3'h0;
+    end else if ( en ) begin
+      cnt_nxt = cnt + 3'b1;
+    end else begin
+      cnt_nxt = 3'h0;
+    end
+  end
+  // ------------
+
+  // ------------
+  // SELECT FREQ AND CREATE STROBE
+  always @ ( * ) begin
+    case ( sel )
+      3'b000  : sel_cnt = 3'h7;
+      3'b001  : sel_cnt = 3'h5;
+      3'b010  : sel_cnt = 3'h3;
+      3'b011  : sel_cnt = 3'h1;
+      default : sel_cnt = 3'h0;
+    endcase // case ( sel )
+  end // always @ ( * )
+
+  always @ ( posedge clk or negedge rst_n ) begin
+    if ( rst_n == 1'b0 ) begin
+      strobe <= 1'b0;
+    end else begin
+      strobe <= (cnt == sel_cnt);
+    end
+  end // always @ ( posedge clk or negedge rst_n )
+  // ------------
+
+  // ------------
+  // REFERENCE CLOCK GENERATE FOR EVERY PHASE MODE
+  always @ ( posedge clk or negedge rst_n ) begin
+    if ( rst_n == 1'b0 ) begin
+      ref_cpha_0 <= 1'b0;
+    end else if ( sclk_en && strobe ) begin
+      ref_cpha_0 <= !ref_cpha_0;
+    end
+  end 
+
+  always @ ( posedge clk or negedge rst_n ) begin
+    if ( rst_n == 1'b0 ) begin
+      ref_cpha_1 <= 1'b0;
+    end else if ( strobe ) begin
+      ref_cpha_1 <= ref_cpha_0;
+    end
+  end
+  // ------------
+
+  // ------------
+  // SPI CLOCK GENERATE
+  assign ref_clk = cpha ? ref_cpha_1 : ref_cpha_0;
+  assign sclk = cpol ? ref_clk : !ref_clk;
+  assign fall =  ref_cpha_0 && strobe;
+  assign rise = !ref_cpha_0 && strobe;
+  // ------------
+
+  /*
   reg [2:0] clk_cnt;
   reg [2:0] sel_cnt;
   
@@ -104,7 +175,7 @@ module baud_rate_gen
   assign sclk   = cpha ? sclk_w : tmp;
   
   assign fall = sclk_reg && strobe;
-  assign rise = !sclk_reg && strobe;
+  assign rise = !sclk_reg && strobe;*/
   // ------------
 
 endmodule // baud_rate_gen
