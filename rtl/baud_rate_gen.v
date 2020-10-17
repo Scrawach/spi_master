ef1f19c33094ac42981099c42571e89751bd810a
Scrawach <Scrawach@gmail.com>
2020-10-17 21:29:28 +0300

    Deleted temporaries modules.
    Fast refactoring by baud rate generator.
    Deleted sync and div_clk modules.

--- rtl/baud_rate_gen.v
+++ rtl/baud_rate_gen.v
@@ -26,18 +26,19 @@ module baud_rate_gen
   // Internal wires
   wire       cpol;    // clock polarity (selected SPI mode)
   wire       cpha;    // clock phase    (selected SPI mode)
-  wire       ref_clk;
+  wire       ref_clk; // reference (gold) clock for generate SCLK
+  
   // ------------
   // Internal registers
-  reg [2:0] cnt, cnt_nxt;
-  reg [2:0] sel_cnt;
-  reg       strobe;
-  reg       ref_cpha_0; // reference 50% duty cycle clock by selected strobes (pha = 0)
-  reg       ref_cpha_1; // reference 50% duty cycle clock by selected strobes (pha = 1)
-
+  reg [2:0] cnt, cnt_nxt; // simple counter
+  reg [2:0] sel_cnt;      // selected counter number (for freq)
+  reg       strobe;       // strobe for generate reference clock
+  reg       ref_cpha_0;   // reference 50% duty cycle clock by selected strobes (pha = 0)
+  reg       ref_cpha_1;   // reference 50% duty cycle clock by selected strobes (pha = 1)
   
   // ------------
   // MODULE IMPLEMENTATION
+  // ------------
 
   // ------------
   // Useful assigns
@@ -52,7 +53,7 @@ module baud_rate_gen
     end else begin
       cnt <= cnt_nxt;
     end
-  end
+  end // always @ ( posedge clk or negedge rst_n ) 
 
   always @ ( * ) begin
     if ( strobe      ) begin
@@ -62,7 +63,7 @@ module baud_rate_gen
     end else begin
       cnt_nxt = 3'h0;
     end
-  end
+  end // always @ ( * )
   // ------------
 
   // ------------
@@ -94,7 +95,7 @@ module baud_rate_gen
     end else if ( sclk_en && strobe ) begin
       ref_cpha_0 <= !ref_cpha_0;
     end
-  end 
+  end // always @ ( posedge clk or negedge rst_n )
 
   always @ ( posedge clk or negedge rst_n ) begin
     if ( rst_n == 1'b0 ) begin
@@ -102,7 +103,7 @@ module baud_rate_gen
     end else if ( strobe ) begin
       ref_cpha_1 <= ref_cpha_0;
     end
-  end
+  end // always @ ( posedge clk or negedge rst_n )
   // ------------
 
   // ------------
@@ -111,71 +112,6 @@ module baud_rate_gen
   assign sclk = cpol ? ref_clk : !ref_clk;
   assign fall =  ref_cpha_0 && strobe;
   assign rise = !ref_cpha_0 && strobe;
-  // ------------
-
-  /*
-  reg [2:0] clk_cnt;
-  reg [2:0] sel_cnt;
-  
-  reg       clk_en;
-  reg       tmp;
-  reg       strobe_r;
-        
-  always @ ( posedge clk or negedge rst_n ) begin
-    if ( rst_n == 1'b0 ) begin
-      clk_cnt <= 3'h0;
-    end else if ( strobe ) begin
-      clk_cnt <= 3'h0;
-    end else if ( en   ) begin
-      clk_cnt <= clk_cnt + 3'h1;
-    end
-  end
-
-  always @ ( * ) begin
-    case ( sel )
-      3'b000 : sel_cnt = 3'h7;
-      3'b001 : sel_cnt = 3'h5;
-      3'b010 : sel_cnt = 3'h3;
-      3'b011 : sel_cnt = 3'h1;
-      default : sel_cnt = 3'h0;
-    endcase // case ( sel )
-  end
-
-  assign strobe_w = ( clk_cnt == sel_cnt );
-
-  always @ ( posedge clk or negedge rst_n ) begin
-    if ( rst_n == 1'b0 ) begin
-      strobe_r <= 1'b0;
-    end else begin
-      strobe_r <= strobe_w;
-    end
-  end
-
-  assign strobe = strobe_r;
-  
-
-  always @ ( posedge clk or negedge rst_n ) begin
-    if ( rst_n == 1'b0 ) begin
-      sclk_reg <= 1'b0;
-    end else if ( sclk_en && strobe ) begin
-      sclk_reg <= !sclk_reg;
-    end
-  end
-
-  always @ ( posedge clk or negedge rst_n ) begin
-    if ( rst_n == 1'b0 ) begin
-      tmp <= cpol;
-    end else if ( strobe ) begin
-      tmp <= sclk_w;
-    end
-  end
-        
-
-  assign sclk_w = cpol ? !sclk_reg : sclk_reg;
-  assign sclk   = cpha ? sclk_w : tmp;
-  
-  assign fall = sclk_reg && strobe;
-  assign rise = !sclk_reg && strobe;*/
   // ------------
 
 endmodule // baud_rate_gen
